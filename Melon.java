6f258d376e8eb2d147764cb051439b0a09753f66
Matthew Guizar <matthewrguizar@gmail.com>
2022-05-03 13:03:44 -0700

    finished melon practice

---
+++ Melon.java
@@ -0,0 +1,31 @@
+public class Melon {
+
+	public static void main(String[] args) {
+		System.out.println("melon defined");
+	}
+
+	MelonType melonType;
+	int shapeRating;
+	int colorRating;
+	int field;
+	String harvester;
+
+	public Melon(MelonType melonType,
+				 int shapeRating,
+				 int colorRating,
+				 int field,
+				 String harvester) {
+		this.melonType = melonType;
+		this.shapeRating = shapeRating;
+		this.colorRating = colorRating;
+		this.field = field;
+		this.harvester = harvester;
+	}
+
+	public boolean isSellable() {
+		return this.shapeRating > 5 & this.colorRating > 5 & this.field != 3;
+	}
+}
+
+
+
